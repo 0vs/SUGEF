deec242dfb8b4a7e86e3abde2a700befc78788b3
Andorinha Nervosa <50371395+AndorinhaNervosa@users.noreply.github.com>
2019-11-11 11:25:08 -0300

    CADASTRO BOVINO

---
+++ Cadastro_bovino/cadastrobov_files/formoid1/form.php
@@ -0,0 +1,36 @@
+<?php
+
+define('EMAIL_FOR_REPORTS', 'aisla_livia1785@hotmail.com');
+define('RECAPTCHA_PRIVATE_KEY', '@privatekey@');
+define('FINISH_URI', 'http://');
+define('FINISH_ACTION', 'message');
+define('FINISH_MESSAGE', 'Cadastrado com sucesso!');
+define('UPLOAD_ALLOWED_FILE_TYPES', 'doc, docx, xls, csv, txt, rtf, html, zip, jpg, jpeg, png, gif');
+
+define('_DIR_', str_replace('\\', '/', dirname(__FILE__)) . '/');
+require_once _DIR_ . '/handler.php';
+
+?>
+
+<?php if (frmd_message()): ?>
+<link rel="stylesheet" href="<?php echo dirname($form_path); ?>/formoid-solid-light-green.css" type="text/css" />
+<span class="alert alert-success"><?php echo FINISH_MESSAGE; ?></span>
+<?php else: ?>
+<!-- Start Formoid form-->
+<link rel="stylesheet" href="<?php echo dirname($form_path); ?>/formoid-solid-light-green.css" type="text/css" />
+<script type="text/javascript" src="<?php echo dirname($form_path); ?>/jquery.min.js"></script>
+<form class="formoid-solid-light-green" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post"><div class="title"><h2>Cadastrar Animal</h2></div>
+	<div class="element-input<?php frmd_add_class("input"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input" placeholder="Nome"/><span class="icon-place"></span></div></div>
+	<div class="element-number<?php frmd_add_class("number"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="text" min="0" max="100" name="number" placeholder="Código" value=""/><span class="icon-place"></span></div></div>
+	<div class="element-date<?php frmd_add_class("date"); ?>"><label class="title"></label><div class="item-cont"><input class="large" data-format="yyyy-mm-dd" type="date" name="date" placeholder="Data de Nascimento"/><span class="icon-place"></span></div></div>
+	<div class="element-input<?php frmd_add_class("input2"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input2" placeholder="Fase"/><span class="icon-place"></span></div></div>
+	<div class="element-input<?php frmd_add_class("input3"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input3" placeholder="Peso"/><span class="icon-place"></span></div></div>
+	<div class="element-number<?php frmd_add_class("number2"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="text" min="0" max="100" name="number2" placeholder="Código Mãe" value=""/><span class="icon-place"></span></div></div>
+	<div class="element-radio<?php frmd_add_class("radio"); ?>"><label class="title">Sexo</label>		<div class="column column1"><label><input type="radio" name="radio" value="Fêmea" /><span>Fêmea</span></label><label><input type="radio" name="radio" value="Macho" /><span>Macho</span></label></div><span class="clearfix"></span>
+</div>
+<div class="submit"><input type="submit" value="Cadastrar"/></div></form><script type="text/javascript" src="<?php echo dirname($form_path); ?>/formoid-solid-light-green.js"></script>
+
+<!-- Stop Formoid form-->
+<?php endif; ?>
+
+<?php frmd_end_form(); ?>
