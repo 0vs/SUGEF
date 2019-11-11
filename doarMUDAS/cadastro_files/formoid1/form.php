8ff23d401d784062c1d4be02f26f8db56da108c1
Andorinha Nervosa <50371395+AndorinhaNervosa@users.noreply.github.com>
2019-11-11 11:47:50 -0300

    DOAR VIVEIRO

---
+++ doarMUDAS/cadastro_files/formoid1/form.php
@@ -0,0 +1,36 @@
+<?php
+
+define('EMAIL_FOR_REPORTS', '');
+define('RECAPTCHA_PRIVATE_KEY', '@privatekey@');
+define('FINISH_URI', 'http://');
+define('FINISH_ACTION', 'message');
+define('FINISH_MESSAGE', 'Thanks for filling out my form!');
+define('UPLOAD_ALLOWED_FILE_TYPES', 'doc, docx, xls, csv, txt, rtf, html, zip, jpg, jpeg, png, gif');
+
+define('_DIR_', str_replace('\\', '/', dirname(__FILE__)) . '/');
+require_once _DIR_ . '/handler.php';
+
+?>
+
+<?php if (frmd_message()): ?>
+<link rel="stylesheet" href="<?php echo dirname($form_path); ?>/formoid-solid-light-green.css" type="text/css" />
+<span class="alert alert-success"><?php echo FINISH_MESSAGE; ?></span>
+<?php else: ?>
+<!-- Start Formoid form-->
+<link rel="stylesheet" href="<?php echo dirname($form_path); ?>/formoid-solid-light-green.css" type="text/css" />
+<script type="text/javascript" src="<?php echo dirname($form_path); ?>/jquery.min.js"></script>
+<form class="formoid-solid-light-green" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post"><div class="title"><h2>Doar muda(s)</h2></div>
+	<div class="element-input<?php frmd_add_class("input"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input" placeholder="Matrícula do bolsista"/><span class="icon-place"></span></div></div>
+	<div class="element-input<?php frmd_add_class("input1"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input1" placeholder="Nome do donatário"/><span class="icon-place"></span></div></div>
+	<div class="element-number<?php frmd_add_class("number"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="text" min="0" max="100" name="number" placeholder="Quantidade de mudas" value=""/><span class="icon-place"></span></div></div>
+	<div class="element-input<?php frmd_add_class("input2"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input2" placeholder="Espécie da muda"/><span class="icon-place"></span></div></div>
+	<div class="element-radio<?php frmd_add_class("radio"); ?>"><label class="title">Tipo</label>		<div class="column column1"><label><input type="radio" name="radio" value="Florestal" /><span>Florestal</span></label><label><input type="radio" name="radio" value="Frutífera" /><span>Frutífera</span></label><label><input type="radio" name="radio" value="Medicinal" /><span>Medicinal</span></label><label><input type="radio" name="radio" value="Ornamental" /><span>Ornamental</span></label><label><input type="radio" name="radio" value="Outra" /><span>Outra</span></label></div><span class="clearfix"></span>
+</div>
+	<div class="element-textarea<?php frmd_add_class("textarea"); ?>"><label class="title"></label><div class="item-cont"><textarea class="medium" name="textarea" cols="20" rows="5" placeholder="Text Area"></textarea><span class="icon-place"></span></div></div>
+	<div class="element-date<?php frmd_add_class("date"); ?>"><label class="title"></label><div class="item-cont"><input class="large" data-format="yyyy-mm-dd" type="date" name="date" placeholder="Date"/><span class="icon-place"></span></div></div>
+<div class="submit"><input type="submit" value="Doar"/></div></form><script type="text/javascript" src="<?php echo dirname($form_path); ?>/formoid-solid-light-green.js"></script>
+
+<!-- Stop Formoid form-->
+<?php endif; ?>
+
+<?php frmd_end_form(); ?>
